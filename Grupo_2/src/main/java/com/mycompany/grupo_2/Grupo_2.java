082391a8ca1b0fc84eedfe0b36b2f144020f5d1c
JosueCarrasco <josue.carrasco@cloud.uautonoma.cl>
2026-06-16 21:47:13 -0400

    testcommit

--- Grupo_2/src/main/java/com/mycompany/grupo_2/Grupo_2.java
+++ Grupo_2/src/main/java/com/mycompany/grupo_2/Grupo_2.java
@@ -1,6 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+
 
 package com.mycompany.grupo_2;
 
@@ -12,5 +10,6 @@ public class Grupo_2 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("lsahdisaugfiusdhfisdf");
     }
 }
