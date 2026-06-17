4fd03d61d13e336f8eb0d7c1b65086ef94663b4e
NightWolfWN <156539693+NightWolfWN@users.noreply.github.com>
2026-06-16 22:14:16 -0400

    v1a nodoreclamos

--- Grupo_2/src/main/java/com/mycompany/grupo_2/NodoReclamos.java
+++ Grupo_2/src/main/java/com/mycompany/grupo_2/NodoReclamos.java
@@ -10,4 +10,35 @@ package com.mycompany.grupo_2;
  */
 public class NodoReclamos {
     
+    private Reclamo reclamo;
+    private NodoReclamos izquierdo;
+    private NodoReclamos derecho;
+
+    public NodoReclamos(Reclamo reclamo) {
+        this.reclamo = reclamo;
+        this.izquierdo = null;
+        this.derecho = null;
+    }
+
+    public Reclamo getReclamo() {
+        return reclamo;
+    }
+
+    public NodoReclamos getIzquierdo() {
+        return izquierdo;
+    }
+
+    public void setIzquierdo(NodoReclamos izquierdo) {
+        this.izquierdo = izquierdo;
+    }
+
+    public NodoReclamos getDerecho() {
+        return derecho;
+    }
+
+    public void setDerecho(NodoReclamos derecho) {
+        this.derecho = derecho;
+    
+    }
 }
+
