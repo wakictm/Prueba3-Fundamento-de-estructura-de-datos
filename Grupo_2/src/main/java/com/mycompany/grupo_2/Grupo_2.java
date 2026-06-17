5e27d0f27f27fa452051f5869ac93d22eb41dd1f
Joaquín Wylie <joaco.andreswylie@gmail.com>
2026-06-16 20:12:55 -0400

    Inicio del proyecto

---
+++ Grupo_2/src/main/java/com/mycompany/grupo_2/Grupo_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.grupo_2;
+
+/**
+ *
+ * @author wakictm
+ */
+public class Grupo_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
